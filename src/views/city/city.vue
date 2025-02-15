3ade674794fd6934709a7e5db98d86e30926694b
陆小果 <luxiaoguo@foxmail.com>
2025-02-15 19:58:42 +0800

    配置axios，city

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -7,21 +7,42 @@
           show-action
           @cancel='cancelClick'
           />
+          <van-tabs v-model:active="activeTab" color="var(--primary-color)">
+              <van-tab title="国内·港澳台">
 
+              </van-tab>
+             <van-tab title="海外">
+
+             </van-tab>
+           </van-tabs>
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { getCityAll } from '@/services/index'
+
 const router = useRouter()
 
+// 1.搜索框功能
+const searchValue = ref('')
 const cancelClick = () => {
     router.back()
 }
 
-// 搜索框功能
-const searchValue = ref('')
+// 2.标签页功能
+const activeTab = ref()
+
+// 网络请求
+// HYRequest.get({
+//     url: '/city/all'
+// }).then(res => {
+//     console.log(res)
+// })
+getCityAll().then(res => {
+    console.log(res)
+})
 </script>
 
 <style lang="less" scoped>
